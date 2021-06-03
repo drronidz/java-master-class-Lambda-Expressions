6c6028e835b3d527d68c97861faa71e34033dd76
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-06-03 16:28:02 +0100

    issue done. closes #6.

--- Employees/src/com/drronidz/Main.java
+++ Employees/src/com/drronidz/Main.java
@@ -5,9 +5,13 @@ package com.drronidz;/*
     CREATED ON : 3:34 PM
 */
 
+import java.lang.ref.PhantomReference;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,8 +21,8 @@ public class Main {
         Employee tim = new Employee("Tim Buchalka", 21);
         Employee sam = new Employee("Sam Loe", 40);
         Employee snow = new Employee("Snow White", 22);
-        Employee show = new Employee("Show Luck",26);
-        Employee cavani = new Employee("Edison Cavani",34);
+        Employee show = new Employee("Show Luck", 26);
+        Employee cavani = new Employee("Edison Cavani", 34);
 
         List<Employee> employees = new ArrayList<>();
         employees.add(john);
@@ -28,11 +32,42 @@ public class Main {
         employees.add(show);
         employees.add(cavani);
 
-        printEmployeeByAge(employees, "Employee over 30", employee -> employee.getAge() > 30);
-        printEmployeeByAge(employees, "\nEmployee 30 and under", employee -> employee.getAge() <= 30);
+        printEmployeeByAge(employees, "Employee over 30",
+                employee -> employee.getAge() > 30);
 
+        printEmployeeByAge(employees, "\nEmployee 30 and under",
+                employee -> employee.getAge() <= 30);
 
+        printEmployeeByAge(employees, "\nEmployees younger than 25",
+                new Predicate<Employee>() {
+                    @Override
+                    public boolean test(Employee employee) {
+                        return employee.getAge() < 25;
+                    }
+                });
 
+        IntPredicate greaterThan15 = i -> i > 15;
+        IntPredicate lessThan100 = i -> i < 100;
+
+        System.out.println("====================================");
+        System.out.println(greaterThan15.test(10));
+        int a = 20;
+        System.out.println(greaterThan15.test(a + 5));
+
+        System.out.println("====================================");
+        System.out.println(greaterThan15.or(lessThan100).test(10));
+
+        System.out.println("====================================");
+        Random random = new Random();
+        for(int i=0; i<10; i++) {
+            System.out.println(random.nextInt(1000));
+        }
+
+        System.out.println("====================================");
+        Supplier<Integer> randomSupplier = () -> random.nextInt(1000);
+        for(int i=0; i<10; i++) {
+            System.out.println(randomSupplier.get());
+        }
     }
 
     private static void printEmployeeByAge(List<Employee> employees,
@@ -40,8 +75,8 @@ public class Main {
                                            Predicate<Employee> ageCondition) {
         System.out.println(ageText);
         System.out.println("====================================");
-        for(Employee employee : employees) {
-            if(ageCondition.test(employee)) {
+        for (Employee employee : employees) {
+            if (ageCondition.test(employee)) {
                 System.out.println(employee.getName());
             }
         }

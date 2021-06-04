7eb1d660aac8ccbe6993b0b0a58cc679e004add3
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-06-04 14:44:06 +0100

    issue done. closes #13.

--- Challenge/src/com/dronidz/Main.java
+++ Challenge/src/com/dronidz/Main.java
@@ -5,8 +5,13 @@ package com.dronidz;/*
     CREATED ON : 1:46 PM
 */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,7 +21,7 @@ public class Main {
             public void run() {
                 String myString = "Let's split this p into an array";
                 String[] parts = myString.split(" ");
-                for(String part: parts) {
+                for (String part : parts) {
                     System.out.println(part);
                 }
             }
@@ -27,7 +32,7 @@ public class Main {
         Runnable runnableTwo = () -> {
             String myString = "Let's split this p into an array";
             String[] parts = myString.split(" ");
-            for(String part: parts) {
+            for (String part : parts) {
                 System.out.println(part);
             }
         };
@@ -36,8 +41,8 @@ public class Main {
 
         Function<String, String> lambdaFunction = (String source) -> {
             StringBuilder returnVal = new StringBuilder();
-            for (int i = 0; i < source.length() ; i++) {
-                if(i % 2 == 1) {
+            for (int i = 0; i < source.length(); i++) {
+                if (i % 2 == 1) {
                     returnVal.append(source.charAt(i));
                 }
             }
@@ -48,17 +53,71 @@ public class Main {
         System.out.println(lambdaFunction.apply("1234567890"));
 //        System.out.println(everySecondCharacter(lambdaFunction,"1234567890"));
 
-        String result = everySecondCharacter(lambdaFunction,"1234567890");
+        String result = everySecondCharacter(lambdaFunction, "1234567890");
         System.out.println(result);
 
         //
 //        Supplier<String> iLoveJava = () -> "I love Java!";
         Supplier<String> iLoveJava = () ->
-        {return "I love Java!";};
-
+        {
+            return "I love Java!";
+        };
+        System.out.println("\n-----------------------------------------");
         //
         String supplierResult = iLoveJava.get();
         System.out.println(supplierResult);
+
+        //
+        System.out.println("\n-----------------------------------------");
+
+        List<String> bestNames = Arrays.asList(
+                "Amelia",
+                "Olivia",
+                "emily",
+                "Isla",
+                "Ava",
+                "olivier",
+                "Jack",
+                "Charlie",
+                "harry",
+                "Jacob"
+        );
+
+        List<String> firstUpperCaseList = new ArrayList<>();
+        bestNames.forEach(name ->
+                firstUpperCaseList.add(name.substring(0, 1).toUpperCase() + name.substring(1)));
+//        firstUpperCaseList.sort((stringOne, stringTwo) -> stringOne.compareTo(stringTwo));
+//        firstUpperCaseList.sort(Comparator.naturalOrder());
+        firstUpperCaseList.sort(String::compareTo);
+//        firstUpperCaseList.forEach(s -> System.out.println(s));
+        firstUpperCaseList.forEach(System.out::println);
+
+        System.out.println("\n-----------------------------------------");
+
+        bestNames
+                .stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .sorted(String::compareTo)
+                .forEach(System.out::println);
+
+        System.out.println("\n-----------------------------------------");
+
+        long namesBeginningWithA = bestNames
+                .stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .filter(name -> name.startsWith("A"))
+                .count();
+
+        System.out.println("Number of names beginning with A is: " + namesBeginningWithA);
+
+        System.out.println("\n-----------------------------------------");
+
+        bestNames
+                .stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .peek(System.out::println)
+                .sorted(String::compareTo)
+                .collect(Collectors.toList());
 
 
     }
@@ -66,8 +125,8 @@ public class Main {
     //
     public static String everySecondChar(String source) {
         StringBuilder returnVal = new StringBuilder();
-        for (int i = 0; i < source.length() ; i++) {
-            if(i % 2 == 1) {
+        for (int i = 0; i < source.length(); i++) {
+            if (i % 2 == 1) {
                 returnVal.append(source.charAt(i));
             }
         }
@@ -75,7 +134,7 @@ public class Main {
         return returnVal.toString();
     }
 
-    public static String everySecondCharacter(Function<String, String> function, String source){
+    public static String everySecondCharacter(Function<String, String> function, String source) {
         return function.apply(source);
     }
 }
